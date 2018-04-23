383feb6d0c672a07dd1e54d4711c8c0277604835
Jennifer Wilcox <jennifer@nitori.nl>
2018-04-23 12:26:06 -0500

    Add a hack for seeing the char data

--- src/emu/lcd.rs
+++ src/emu/lcd.rs
@@ -20,9 +20,17 @@ pub const SCREEN_SIZE: (usize, usize) = (160, 144);
 pub struct Pixel(pub u8, pub u8, pub u8, pub u8);
 
 const COLOR_WHITE: Pixel = Pixel(234, 255, 186, 255);
+const COLOR_LIGHT_GRAY: Pixel = Pixel(150, 187, 146, 255);
+const COLOR_DARK_GRAY: Pixel = Pixel(68, 106, 81, 255);
+const COLOR_BLACK: Pixel = Pixel(0, 14, 2, 255);
+const COLORS: [Pixel; 4] = [COLOR_WHITE, COLOR_LIGHT_GRAY, COLOR_DARK_GRAY, COLOR_BLACK];
+
 const LINE_CYCLE_TIME: u64 = CLOCK_RATE * 180_700 / 1_000_000_000;
+const BYTES_PER_CHAR: u16 = 16;
+const BYTES_PER_CHAR_ROW: u16 = 2;
 
 pub type Framebuffer = [[Pixel; SCREEN_SIZE.0]; SCREEN_SIZE.1];
+type CharRow = [usize; 8];
 
 pub struct Lcd {
     lcdc: u8,
@@ -107,6 +115,22 @@ impl Lcd {
     }
 
     pub fn do_hblank(&mut self, cycle: u64) {
+        // TODO: This is just a debug hack to display char data on the screen
+        const CHARS_PER_ROW: u8 = (SCREEN_SIZE.0 as u8 / 8);
+        let current_char_start = (self.ly as u16 * CHARS_PER_ROW as u16 / 8) as u8;
+        if current_char_start < (255 - CHARS_PER_ROW) && self.ly < SCREEN_SIZE.1 as u8 {
+            for i in 0..CHARS_PER_ROW {
+                let char_ = current_char_start + i;
+                let row = self.read_char_row_at(char_, self.ly % 8, true);
+                for j in 0..8 {
+                    let x = (i + j) as usize;
+                    let y = self.ly as usize;
+                    let color_index = row[j as usize] as usize;
+                    self.get_back_framebuffer()[y][x] = COLORS[color_index];
+                }
+            }
+        }
+
         self.ly += 1;
         self.next_hblank_cycle = LINE_CYCLE_TIME + cycle;
     }
@@ -116,6 +140,28 @@ impl Lcd {
         self.ly = 0;
         self.next_vblank_cycle = 154 * LINE_CYCLE_TIME + cycle;
     }
+
+    fn read_char_row_at(&self, char_: u8, row: u8, high: bool) -> CharRow {
+        let base_address = if high {
+            Address(0x9000)
+        } else {
+            Address(0x8000)
+        };
+        let char_address = base_address + Address(BYTES_PER_CHAR * (char_ as u16));
+        let row_address = char_address + Address(BYTES_PER_CHAR_ROW * (row as u16));
+        let b1 = self.read(row_address).unwrap();
+        let b2 = self.read(row_address + Address(1)).unwrap();
+        let mut row = [0; 8];
+        for i in 0..8 {
+            row[i] = (read_bit(b1, i as u8) | (read_bit(b2, i as u8) << 1)) as usize;
+        }
+        row
+    }
+}
+
+fn read_bit(value: u8, bit: u8) -> u8 {
+    let mask = 1 << bit;
+    (value & mask) >> bit
 }
 
 impl MemDevice for Lcd {
