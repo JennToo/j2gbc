bec173b68b0e2b8aadcd3799bc1798c7fd7d5965
Jennifer Wilcox <jennifer@nitori.nl>
2018-05-03 12:29:49 -0500

    Refactor screen rendering some

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,21 +1,19 @@
 use sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::Texture;
+use sdl2::pixels::Color;
 use std::time::{Duration, Instant};
 
 use emu::system::System;
-use emu::lcd::{Framebuffer, SCREEN_SIZE};
+use emu::lcd::SCREEN_SIZE;
+
+mod fb;
+
+use self::fb::{Framebuffers, RenderingState};
 
 pub struct Window {
     ctx: sdl2::Sdl,
     window_canvas: sdl2::render::WindowCanvas,
-    rendering_state: RenderingState,
-}
-
-enum RenderingState {
-    LcdFramebuffer,
-    CharDat(bool),
 }
 
 impl Window {
@@ -26,6 +24,8 @@ impl Window {
             video
                 .window("j2gbc", 800, (800 * SCREEN_SIZE.1 / SCREEN_SIZE.0) as u32)
                 .position_centered()
+                .resizable()
+                .maximized()
                 .build()
                 .map_err(|e| format!("{}", e))
         );
@@ -37,25 +37,12 @@ impl Window {
                 .map_err(|e| format!("{}", e))
         );
 
-        Ok(Window {
-            ctx,
-            window_canvas,
-            rendering_state: RenderingState::LcdFramebuffer,
-        })
+        Ok(Window { ctx, window_canvas })
     }
 
     pub fn run(&mut self, mut system: System) -> Result<(), String> {
         let texture_creator = self.window_canvas.texture_creator();
-        let mut gb_screen = try!(
-            texture_creator
-                .create_texture_streaming(
-                    sdl2::pixels::PixelFormatEnum::RGBA8888,
-                    SCREEN_SIZE.0 as u32,
-                    SCREEN_SIZE.1 as u32
-                )
-                .map_err(|e| format!("{}", e))
-        );
-
+        let mut fbs = Framebuffers::new(&texture_creator)?;
         let mut dt = Instant::now();
 
         loop {
@@ -71,28 +58,17 @@ impl Window {
                         keycode: Some(Keycode::F1),
                         ..
                     } => {
-                        self.rendering_state = RenderingState::LcdFramebuffer;
+                        fbs.rendering_state = RenderingState::Normal;
                     }
-
                     Event::KeyDown {
                         keycode: Some(Keycode::F2),
                         ..
                     } => {
-                        self.rendering_state = RenderingState::CharDat(false);
+                        fbs.rendering_state = RenderingState::Debug;
                     }
-
-                    Event::KeyDown {
-                        keycode: Some(Keycode::F3),
-                        ..
-                    } => {
-                        self.rendering_state = RenderingState::CharDat(true);
-                    }
-
                     _ => {}
                 }
             }
-
-            self.window_canvas.clear();
 
             let elapsed = dt.elapsed();
             if elapsed > Duration::from_millis(17) {
@@ -101,41 +77,10 @@ impl Window {
             system.run_for_duration(&elapsed);
             dt = Instant::now();
 
-            try!(self.render(&system, &mut gb_screen));
-
-            try!(self.window_canvas.copy(&gb_screen, None, None));
+            self.window_canvas.set_draw_color(Color::RGB(100, 100, 100));
+            try!(self.window_canvas.fill_rect(None));
+            try!(fbs.render(&system, &mut self.window_canvas));
             self.window_canvas.present();
         }
     }
-
-    fn render<'r>(&mut self, system: &System, gb_screen: &mut Texture<'r>) -> Result<(), String> {
-        match self.rendering_state {
-            RenderingState::LcdFramebuffer => {
-                let fb = system.get_framebuffer();
-                copy_framebuffer(fb, gb_screen)
-            }
-            RenderingState::CharDat(high) => {
-                let fb = system.cpu.mmu.lcd.render_char_dat(high);
-                copy_framebuffer(&fb, gb_screen)
-            }
-        }
-    }
-}
-
-fn copy_framebuffer<'r>(fb: &Framebuffer, gb_screen: &mut Texture<'r>) -> Result<(), String> {
-    gb_screen
-        .with_lock(None, |outfb, _| {
-            for y in 0..SCREEN_SIZE.1 {
-                for x in 0..SCREEN_SIZE.0 {
-                    let index = 4 * x + 4 * y * SCREEN_SIZE.0;
-
-                    let pixel = fb[y][x];
-                    outfb[index] = pixel.3;
-                    outfb[index + 1] = pixel.2;
-                    outfb[index + 2] = pixel.1;
-                    outfb[index + 3] = pixel.0;
-                }
-            }
-        })
-        .map_err(|e| format!("{}", e))
 }
