2425228015345b1c6d3f70a29b449b59192cf180
Jennifer Wilcox <jennifer@nitori.nl>
2018-05-12 16:35:52 -0500

    Fix order for the subtract instructions

--- src/emu/cpu/mod.rs
+++ src/emu/cpu/mod.rs
@@ -148,21 +148,21 @@ impl Cpu {
                 self[Register8::F] = flags.0;
             }
             Arith::SubN => {
-                let v1 = try!(self.read_indirect(Register16::HL));
-                let v2 = self[Register8::A];
+                let v1 = self[Register8::A];
+                let v2 = try!(self.read_indirect(Register16::HL));
                 let (v, flags) = sub(v1, v2);
                 self[Register8::A] = v;
                 self[Register8::F] = flags.0;
             }
             Arith::SubR(r) => {
-                let v1 = self[r];
-                let v2 = self[Register8::A];
+                let v1 = self[Register8::A];
+                let v2 = self[r];
                 let (v, flags) = sub(v1, v2);
                 self[Register8::A] = v;
                 self[Register8::F] = flags.0;
             }
-            Arith::SubI(v1) => {
-                let v2 = self[Register8::A];
+            Arith::SubI(v2) => {
+                let v1 = self[Register8::A];
                 let (v, flags) = sub(v1, v2);
                 self[Register8::A] = v;
                 self[Register8::F] = flags.0;
