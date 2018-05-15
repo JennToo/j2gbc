e9f401a7a1f318f98f76a05824f0540dabb3879a
Jennifer Wilcox <jennifer@nitori.nl>
2018-05-14 19:18:12 -0500

    Add RAM switching to MBC1

--- src/emu/mbc/mbc1.rs
+++ src/emu/mbc/mbc1.rs
@@ -1,14 +1,19 @@
 use super::Mbc;
 use emu::mem::{Address, AddressRange, ExtendedAddress, MemDevice, RNG_ROM_BANK1, Ram, RNG_EXT_RAM};
 
-const RNG_BANK_SELECT: AddressRange = AddressRange(Address(0x2000), Address(0x4000));
+const RNG_LOWER_BANK_SELECT: AddressRange = AddressRange(Address(0x2000), Address(0x4000));
 const RNG_RAMCS: AddressRange = AddressRange(Address(0x0000), Address(0x2000));
-const MASK_BANK_SELECT: u8 = 0b0001_1111;
+const RNG_UPPER_BANK_SELECT: AddressRange = AddressRange(Address(0x4000), Address(0x6000));
+const RNG_CTRL_UPPER_BANK_SELECT: AddressRange = AddressRange(Address(0x6000), Address(0x8000));
+const MAKS_UPPER_BANK_SELCET: u8 = 0b0000_0011;
+const MASK_LOWER_BANK_SELECT: u8 = 0b0001_1111;
 
 pub struct Mbc1 {
     ram_protected: bool,
     rom: Vec<u8>,
-    rom_bank: usize,
+    lower_bank_select: usize,
+    upper_bank_controls_rom: bool,
+    upper_bank_select: usize,
     ram: Ram,
 }
 
@@ -17,9 +22,20 @@ impl Mbc1 {
         Mbc1 {
             ram_protected: true,
             rom,
-            rom_bank: 1,
-            ram: Ram::new(RNG_EXT_RAM.len()),
+            upper_bank_controls_rom: true,
+            upper_bank_select: 0,
+            lower_bank_select: 0,
+            ram: Ram::new(RNG_EXT_RAM.len() * 4),
         }
+    }
+
+    fn map_address_into_ram(&self, a: Address) -> Address {
+        let bank = if !self.upper_bank_controls_rom {
+            self.upper_bank_select
+        } else {
+            0
+        };
+        Address(((a - RNG_EXT_RAM.0).0 as usize + RNG_EXT_RAM.len() * bank) as u16)
     }
 }
 
@@ -36,21 +52,25 @@ impl MemDevice for Mbc1 {
     }
 
     fn write(&mut self, a: Address, v: u8) -> Result<(), ()> {
-        if a.in_(RNG_BANK_SELECT) {
-            self.rom_bank = (v & MASK_BANK_SELECT) as usize;
-            if self.rom_bank == 0 {
-                self.rom_bank = 1;
-            }
+        if a.in_(RNG_LOWER_BANK_SELECT) {
+            self.lower_bank_select = (v & MASK_LOWER_BANK_SELECT) as usize;
             Ok(())
         } else if a.in_(RNG_EXT_RAM) {
             if self.ram_protected {
                 error!("Error: RAM is not writable right now");
                 Err(())
             } else {
-                self.ram.write(a - RNG_EXT_RAM.0, v)
+                let mapped = self.map_address_into_ram(a);
+                self.ram.write(mapped, v)
             }
         } else if a.in_(RNG_RAMCS) {
             self.ram_protected = !(v == 0x0A);
+            Ok(())
+        } else if a.in_(RNG_UPPER_BANK_SELECT) {
+            self.upper_bank_select = (v & MAKS_UPPER_BANK_SELCET) as usize;
+            Ok(())
+        } else if a.in_(RNG_CTRL_UPPER_BANK_SELECT) {
+            self.upper_bank_controls_rom = v == 0;
             Ok(())
         } else {
             error!("Unimplemented MBC1 register");
@@ -61,6 +81,10 @@ impl MemDevice for Mbc1 {
 
 impl Mbc for Mbc1 {
     fn map_address_into_rom(&self, a: Address) -> ExtendedAddress {
-        ExtendedAddress((RNG_ROM_BANK1.len() * (self.rom_bank - 1)) as u32 + a.0 as u32)
+        if self.upper_bank_controls_rom && self.upper_bank_select != 0 {
+            unimplemented!();
+        }
+
+        ExtendedAddress((RNG_ROM_BANK1.len() * (self.lower_bank_select - 1)) as u32 + a.0 as u32)
     }
 }
