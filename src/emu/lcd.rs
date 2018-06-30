1e296e057b098ebfe716a2e26c11122c43c5c59b
Jennifer Wilcox <jennifer@nitori.nl>
2018-06-29 21:50:35 -0500

    Fix debug build perf issue by making the code ugly

--- src/emu/lcd.rs
+++ src/emu/lcd.rs
@@ -500,9 +500,13 @@ impl Lcd {
                 (obj.char_, 8)
             };
 
-            for y in 0..hi_y {
+            // This isn't a for y in 0..hi_y because it's super slow
+            // in debug builds for some reason
+            let mut y = 0;
+            while y < hi_y {
                 let full_y = y as isize + obj.y as isize - 16;
                 if full_y > SCREEN_SIZE.1 as isize || full_y < 0 || full_y != self.ly as isize {
+                    y += 1;
                     continue;
                 }
 
@@ -536,6 +540,8 @@ impl Lcd {
                         self.get_back_framebuffer()[full_y as usize][full_x as usize] = color;
                     }
                 }
+
+                y += 1;
             }
         }
     }
