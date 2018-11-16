1305cac3a62343d704f940ae46976754a9291171
Jennifer Wilcox <jennifer@nitori.org>
2018-11-15 20:20:56 -0600

    Implement general purpose HDMA

--- j2gbc/src/mmu.rs
+++ j2gbc/src/mmu.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use log::{error, info};
 
+use alu::hi_lo;
 use audio::{Audio, AudioSink};
 use cart::Cart;
 use input::Input;
@@ -31,6 +32,12 @@ pub struct Mmu {
     pub watchpoints: HashSet<Address>,
 
     exceptions: MmuExceptions,
+
+    hdma1: u8,
+    hdma2: u8,
+    hdma3: u8,
+    hdma4: u8,
+    hdma5: u8,
 }
 
 impl Mmu {
@@ -51,6 +58,12 @@ impl Mmu {
             pedantic: true,
             ram_bank_select: 1,
 
+            hdma1: 0,
+            hdma2: 0,
+            hdma3: 0,
+            hdma4: 0,
+            hdma5: 0,
+
             watchpoints: HashSet::new(),
         }
     }
@@ -68,12 +81,33 @@ impl Mmu {
         Ok(())
     }
 
+    fn hdma(&mut self, src: AddressRange, mut dest: Address) -> Result<(), ()> {
+        let mut src_cursor = src.0;
+        while src_cursor < src.1 {
+            let v = self.read(src_cursor)?;
+            self.write(dest, v)?;
+            dest += Address(1);
+            src_cursor += Address(1);
+        }
+        Ok(())
+    }
+
     fn _read(&self, a: Address) -> Result<u8, ()> {
         if self.watchpoints.contains(&a) {
             info!("Read watchpoint for {:?}", a);
             Err(())
         } else if a == REG_SVBK {
             Ok(self.ram_bank_select as u8)
+        } else if a == REG_HDMA1 {
+            Ok(self.hdma1)
+        } else if a == REG_HDMA2 {
+            Ok(self.hdma2)
+        } else if a == REG_HDMA3 {
+            Ok(self.hdma3)
+        } else if a == REG_HDMA4 {
+            Ok(self.hdma4)
+        } else if a == REG_HDMA5 {
+            Ok(self.hdma5)
         } else if a.in_(RNG_INT_RAM_0) {
             self.internal_ram.read(a - RNG_INT_RAM_0.0)
         } else if a.in_(RNG_INT_RAM_1) {
@@ -128,6 +162,28 @@ impl Mmu {
             Ok(())
         } else if a == REG_DMA {
             self.dma(Address((u16::from(v)) << 8))
+        } else if a == REG_HDMA1 {
+            self.hdma1 = v;
+            Ok(())
+        } else if a == REG_HDMA2 {
+            self.hdma2 = v;
+            Ok(())
+        } else if a == REG_HDMA3 {
+            self.hdma3 = v;
+            Ok(())
+        } else if a == REG_HDMA4 {
+            self.hdma4 = v;
+            Ok(())
+        } else if a == REG_HDMA5 {
+            if v & 0b1000_0000 != 0 {
+                panic!("HDMA HBlank not supported");
+            }
+            let src_start = Address(hi_lo(self.hdma1, self.hdma2) & 0b1111_1111_1111_0000);
+            let src_end = src_start + Address(((v & 0b0111_1111) as u16 + 1) * 16);
+            let dest =
+                Address((hi_lo(self.hdma3, self.hdma4) & 0b0001_1111_1111_0000) | (0b1 << 15));
+            self.hdma5 = 0xFF;
+            self.hdma(AddressRange(src_start, src_end), dest)
         } else if a == REG_KEY1 {
             self.prepared_speed_switch = (0b1 & v) == 1;
             Ok(())
