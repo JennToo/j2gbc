4124668eb5ff2cd506a1810efa9be9f9da7c73b4
Jennifer Wilcox <jennifer@nitori.org>
2019-06-30 17:26:39 -0500

    Hook up registers in the debugger

--- j2gbc_gfx/src/debugger.rs
+++ j2gbc_gfx/src/debugger.rs
@@ -1,5 +1,6 @@
 use enclose::enclose;
 use gtk::prelude::*;
+use j2gbc::debug::Register8;
 
 use crate::SystemRef;
 
@@ -11,6 +12,12 @@ struct Context {
     pause_button: gtk::ToolButton,
     resume_button: gtk::ToolButton,
     step_button: gtk::ToolButton,
+
+    register_af: gtk::Label,
+    register_bc: gtk::Label,
+    register_de: gtk::Label,
+    register_sp: gtk::Label,
+    register_pc: gtk::Label,
 }
 
 pub fn load_debugger(system: &SystemRef) -> gtk::Window {
@@ -31,6 +38,12 @@ pub fn load_debugger(system: &SystemRef) -> gtk::Window {
             context.system.borrow_mut().debugger().resume();
             context.running();
         }));
+    context
+        .step_button
+        .connect_clicked(enclose!((context) move |_| {
+            context.system.borrow_mut().debugger().step();
+            context.halted();
+        }));
 
     window.show_all();
     window
@@ -43,20 +56,60 @@ impl Context {
             pause_button: builder.get_object("pause_button").unwrap(),
             resume_button: builder.get_object("resume_button").unwrap(),
             step_button: builder.get_object("step_button").unwrap(),
+
+            register_af: builder.get_object("register_AF").unwrap(),
+            register_bc: builder.get_object("register_BC").unwrap(),
+            register_de: builder.get_object("register_DE").unwrap(),
+            register_sp: builder.get_object("register_SP").unwrap(),
+            register_pc: builder.get_object("register_PC").unwrap(),
         }
     }
 
     pub fn running(&self) {
         self.resume_button.set_sensitive(false);
         self.step_button.set_sensitive(false);
-
         self.pause_button.set_sensitive(true);
     }
 
     pub fn halted(&self) {
         self.resume_button.set_sensitive(true);
         self.step_button.set_sensitive(true);
-
         self.pause_button.set_sensitive(false);
+
+        self.update_regs();
+    }
+
+    pub fn update_regs(&self) {
+        let mut sys = self.system.borrow_mut();
+        let debug = sys.debugger();
+        self.register_af.set_text(
+            format!(
+                "0x{:02x}{:02x}",
+                debug.read_reg(Register8::A),
+                debug.read_reg(Register8::F)
+            )
+            .as_str(),
+        );
+        self.register_bc.set_text(
+            format!(
+                "0x{:02x}{:02x}",
+                debug.read_reg(Register8::B),
+                debug.read_reg(Register8::C)
+            )
+            .as_str(),
+        );
+        self.register_de.set_text(
+            format!(
+                "0x{:02x}{:02x}",
+                debug.read_reg(Register8::D),
+                debug.read_reg(Register8::E)
+            )
+            .as_str(),
+        );
+
+        self.register_pc
+            .set_text(format!("{}", debug.read_pc(),).as_str());
+        self.register_sp
+            .set_text(format!("{}", debug.read_sp(),).as_str());
     }
 }
